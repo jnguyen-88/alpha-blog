1c6d60a578eaae23fbf101309b007d1296d72b1a
Justin Nguyen <justinhnguyen88@gmail.com>
2018-04-26 08:00:26 +0000

    redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_article, only: [:edit, :destroy, :update, :show]
+
+  
   def index
     @articles = Article.all
   end
@@ -18,18 +21,16 @@ class ArticlesController < ApplicationController
     end
     
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Your shit was DESTROYED!"
     redirect_to articles_path
   end
     
   def edit
-    @article = Article.find(params[:id])
+  
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
     flash[:notice] = "Your shit was successfully updated"
     redirect_to article_path(@article)
@@ -39,12 +40,17 @@ class ArticlesController < ApplicationController
 end
   
   def show
-    @article = Article.find(params[:id])
+  
   end
 end
   
   private
+  
     def article_params
       params.require(:article).permit(:title, :description)
+    end
+    
+    def set_article
+      @article = Article.find(params[:id])
     end
   end
